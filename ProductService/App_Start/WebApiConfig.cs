bdf6e27af7a2faa3e72c2ddc2fc47e9ab74038c8
Katerina034 <blinovakater@yandex.ru>
2019-07-11 23:40:15 +0400

    Создание конечной точки OData

--- ProductService/App_Start/WebApiConfig.cs
+++ ProductService/App_Start/WebApiConfig.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using ProductService.Models;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+
+
 
 namespace ProductService
 {
@@ -9,16 +14,13 @@ namespace ProductService
     {
         public static void Register(HttpConfiguration config)
         {
-            // Конфигурация и службы веб-API
-
-            // Маршруты веб-API
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            // New code:
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
         }
     }
 }
