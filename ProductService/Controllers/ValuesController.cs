c2e6ee2f5f1f9235fbb448a9c52c4919a1b77b36
Katerina034 <blinovakater@yandex.ru>
2019-07-18 18:56:30 +0400

    объединение с ангуляр

---
+++ ProductService/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Calabonga.OperationResults;
+
+namespace ProductService.Controllers {
+
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class ValuesController : ApiController {
+
+        public IHttpActionResult Get() {
+            var items = new List<string>
+            {
+                "www.calabonga.net",
+                "Value 2",
+                "Value 3",
+                "Value 4",
+                "Value 5",
+                "Value 6",
+            };
+            return Ok(OperationResult.CreateResult(items));
+        }
+    }
+}
